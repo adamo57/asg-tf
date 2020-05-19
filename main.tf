87e6c4b5174d080c79eba8b63fa33b19e8e2ebfc
adamo57 <adamouellette57@gmail.com>
2020-05-19 06:10:46 -0400

    Add ec2 + lb

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+    ami-id = "ami-07ebfd5b3428b6f4d" // ubuntu 16.04
+}
+
 resource "aws_vpc" "prod" {
   cidr_block = "10.0.0.0/16"
 
@@ -34,5 +38,40 @@ resource "aws_security_group" "default" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = locals.ami_id
+  instance_type = "t2.micro"
+  availabilitity_zone = "us-east-1c"
+  security_group = aws_security_group.default.id
+  subnet_id = aws_subnet.prod-private-us-east-1c.id
+}
+
+module "lb-logs" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "lb-logs"
+  acl    = ""
+}
+
+resource "aws_lb" "prod-ws-lb" {
+  name               = "prod-ws-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.default.id]
+  subnets            = [var.subnet_public.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = module.lb-logs.aws_s3_bucket.this.bucket
+    prefix  = "prod-ws-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
   }
 }
